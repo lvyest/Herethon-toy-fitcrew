d9b4bc432fe7902512d52d3b045e0134742e5051
KAYOUNG KIM <lvyest.dev@gmail.com>
2024-06-06 23:27:44 +0900

    'text'

---
+++ app.js
@@ -0,0 +1,17 @@
+const crewData = [
+  { id: 1, image: 'baseball1.png', category: '야구', dday: '5', title: '야구 아자아자 크루 (모집 중)', link: '#', region: '서울' },
+  { id: 2, image: 'baseball2.png', category: '테니스', dday: '30', title: '팅팅빵빵 크루 (모집 중)', link: '#', region: '인천' },
+  { id: 3, image: 'baseball3.png', category: '탁구', dday: '20', title: '공go!크루 (모집 중)', link: '#', region: '부산' },
+  { id: 4, image: 'baseball4.png', category: '수영', dday: '5', title: '수달처럼 크루 (모집 중)', link: '#', region: '인천' },
+  { id: 5, image: 'baseball5.png', category: '태권도', dday: '3', title: '힘찬이들 크루 (모집 중)', link: '#', region: '인천' },
+  { id: 6, image: 'baseball6.png', category: '축구', dday: '2', title: '축구소녀들 크루 (모집 중)', link: '#', region: '서울' },
+  { id: 7, image: 'baseball7.png', category: '양궁', dday: '10', title: '양궁 운동 스터디 크룹(모집 중)', link: '#', region: '서울' },
+  { id: 8, image: 'baseball8.png', category: '테니스', dday: '7', title: '빨라지자 크루 (모집 중)', link: '#', region: '인천' },
+  { id: 9, image: 'baseball9.png', category: '달리기', dday: '1', title: '7을 향해 (모집 중)', link: '#', region: '인천' },
+  { id: 10, image: 'baseball10.png', category: '야구', dday: '30', title: '탕탕크루 (모집 중)', link: '#', region: '강원' },
+  { id: 11, image: 'baseball11.png', category: '야구', dday: '20', title: '한화 곧 1등 (모집 중)', link: '#', region: '인천' },
+  { id: 12, image: 'baseball12.png', category: '야구', dday: '25', title: '최강소녀 크루 (모집 중)', link: '#', region: '서울' },
+  { id: 13, image: 'baseball13.png', category: '야구', dday: '3', title: '힘찬 야구 (모집 중)', link: '#', region: '강원' },
+  { id: 14, image: 'baseball14.png', category: '야구', dday: '14', title: '야구 문학 (모집 중)', link: '#', region: '인천' },
+];
+
