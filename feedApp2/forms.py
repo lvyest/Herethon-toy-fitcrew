38bafb8f1b929aaf0881c30c746f170dc2934a4a
KAYOUNG KIM <lvyest.dev@gmail.com>
2024-06-13 23:10:44 +0900

    'end'

--- feedApp2/forms.py
+++ feedApp2/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm, forms
+from django import forms
+from django.forms import ModelForm
 from django.forms.widgets import Textarea
 
 from feedApp2.models import Feed
@@ -10,10 +11,10 @@ class FeedCreationForm(ModelForm):
         model = Feed
         fields = ['image', 'content']
         labels = {
-            'image' : '사진을 선택하세요',
+            'image': '사진을 선택하세요',
             'content': '문구를 입력하세요',
         }
         widgets = {
+            'image': PreviewFileWidget(attrs={'placeholder': '파일 선택해주세요'}),
             'content': Textarea(attrs={'rows': 8, 'cols': 30}),
-            # 'image' : PreviewFileWidget,
         }
